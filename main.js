388813360f0ecf28bb2d276d0c1aab670a9e334f
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-15 16:44:48 -0500

    attempted loop challenge

--- main.js
+++ main.js
@@ -26,3 +26,80 @@ console.log(displaySkill('Painting', true))
 console.log(displaySkill('Being lazy', false))
 console.log(displaySkill('Eating dirt', false))
 console.log(displaySkill('Writing poetry and prose', true))
+
+
+var jobs = [
+{
+comp: '--Springstone Equestiran--Stablehand--Feeding, cleaning stalls, administering medication',
+},
+{
+comp: '--Creative Dog Training--Dog walker/ General care--Walking, basic training, bathing, cleaning kennels',
+},
+{comp: '--Brick & Tin--Bartener--Producing high quality craft cocktails in a professional environment',
+}
+]
+
+
+/*
+var job = [ 
+
+{
+compName: 'Springstone Equestiran',
+compName: 'Creative Dog Training',
+compName:  'Brick & Tin', 
+},
+
+{
+title: 'Stablehand',
+title: 'Dog walker/ General care',
+title:  'Bartener',
+},
+
+{
+descrip: 'Feeding, cleaning stalls, administering medication',
+descrip:  'Walking, basic training, bathing, cleaning kennels',
+descrip:  'Producing high quality craft cocktails in a professional environment',
+}
+]
+
+
+*/
+
+
+
+/*function list(job){
+    
+    var i;
+for (i = 0; i < job.length; i++) {
+    text += job[i] + "";
+}
+console.log(list)
+*/
+
+
+
+/*
+for (var key in jobs){
+    console.log('jobs.${key} = ${jobs.[key]}')
+}
+
+
+var text = "";
+var i;
+for (i = 0; i < compName.length; i++) {
+    text += compName[i] + "";
+}
+
+var text = "";
+var i;
+for (i = 0; i < title.length; i++) {
+    text += title[i] + "";
+}
+
+function work (jobs)
+var text = "";
+var i;
+for (i = 0; i < descrip.length; i++) {
+    text += descrip[i] + "";
+}
+*/
